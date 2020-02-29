8ff21e84199d2626271bad231c1d8864d1ed957b
Luz Maria Vigil <luzmaria.vigil@gmail.com>
2020-02-28 18:41:45 -0800

    finally linked my css and js files successfully

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,30 +6,32 @@ var loss;
 
 
 //between 19 and 120
-var randomNumber= i
+var randomNumber= Math.floor(Math.random()* 100+20);
 
+$("#randomNum").text("Random Number:" +randomNumber);
 
-
-$("#number-to-guess").text(randomNumber);
 
 var counter = 0;
-
 
 var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 var increment = numberOptions[Math.round(Math.random())];
 
-$(".crystal-image").on("click", function() {
-
+$(".number").on("click", function() {
+console.log($(this).val());
   
   counter += increment;
 
+  $("#currentScore").text(counter);
   alert("New score: " + counter);
 
-  if (counter === targetNumber) {
+  if (counter === randomNumber) {
+    wins++;
+    $("#winsText").text("Wins:" + wins)
     alert("You win!");
   }
 
-  else if (counter >= targetNumber) {
+  else if (counter >= randomNumber) {
+    
     alert("You lose!!");
   }
 
